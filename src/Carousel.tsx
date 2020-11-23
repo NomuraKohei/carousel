f095a070c09c3229aae2f04d38c4d45e336c7bf3
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-11-23 18:10:32 +0900

    usestateを追加

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.css';
 
 const Carousel: React.FC = () => {
+  const [scroll, setScroll] = useState(0);
 
   const items = [
     'hoge1hogehogeehogehoge1hogehogeehoge',
@@ -12,12 +13,20 @@ const Carousel: React.FC = () => {
     'hoge6hogehogeehoge',
   ];
 
+  const onKeyRightDown = (event: React.KeyboardEvent): void => {
+    console.log("test");
+  }
 
   return (
     <>
       <div className="Carousel">
         {items.map((value, index) => 
           <div className="Carousel-item" key={index}>{value}</div>
+        )}
+      </div>
+      <div className="Carousel" onKeyPress={(event: React.KeyboardEvent) => onKeyRightDown(event)}>
+        {items.map((value, index) => 
+          <div className="Carousel-item" key={index} onClick={() => {setScroll(index); console.log(scroll);}}>{value}</div>
         )}
       </div>
     </>
